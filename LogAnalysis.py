beec46dac9adb9e7178b628378ea8d6fcce8cce2
Dennis <djohnstts@gmail.com>
2018-05-08 22:43:11 -0400

    Add files via upload

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 # "Log Analysis" for the Log Analysis Project for
 # Udacity Full Stack Developer Nanodegree
 
@@ -20,49 +22,62 @@ onePercentErrorDaysQuery = """SELECT tdate, rate
                             FROM dayerrorpercents
                             WHERE rate > 1"""
 
-def getMostPopularArticles():
+
+def getQueryResults(query):
+
+    """
+    getQueryResults() takes a query string as an
+    arguemet, and executes the query via
+    the psycopg2 library, then returns
+    the results
+    """
     db = psycopg2.connect("dbname=news")
     mycursor = db.cursor()
-    mycursor.execute(mostPopularArticlesQuery)
-    results = mycursor.fetchall()
-    db.close()
-    return results
-
-def getMostPopularAuthors():
-    db = psycopg2.connect("dbname=news")
-    mycursor = db.cursor()
-    mycursor.execute(mostPopularAuthorsQuery)
-    results = mycursor.fetchall()
-    db.close()
-    return results
-
-
-def getOnePercentErrorDays():
-    db = psycopg2.connect("dbname=news")
-    mycursor = db.cursor()
-    mycursor.execute(onePercentErrorDaysQuery)
+    mycursor.execute(query)
     results = mycursor.fetchall()
     db.close()
     return results
 
 def printQueryResults(results, title):
+
+    """
+    printQueryResults() takes a query result and
+    string value as arguemets, and prints out
+    first the query results title, then
+    the query results.
+    """
+    
     print('\n');
     print(title)
     for row in results:
         print("%s - %s views" % (row[0], row[1]))
 
 def printErrorQueryResults(results, title):
+
+    """
+    printErrorQueryResults() takes a query result and
+    string value as arguemets, and prints out
+    first the query results title, then
+    the query results.  Since the error results
+    query needs to be formatted differenty
+    than the other query results, the method
+    was created to handle the differences.
+    """
     print('\n');
     print(title)
     for row in results:
-        print '\t' + '\t' + str(row[0]) + " - " + str(Decimal(row[1]).quantize(Decimal('.01'), rounding=ROUND_UP)) + "%"
+        print '\t' + '\t' + str(row[0]) + " - " \
+        + str(Decimal(row[1]).quantize(Decimal('.01'), rounding=ROUND_UP)) \
+        + "%"
         
 
-articlesResults = getMostPopularArticles()
+articlesResults = getQueryResults(mostPopularArticlesQuery)
 printQueryResults(articlesResults, "\tMost Popular Articles")
 
-authorsResults = getMostPopularAuthors()
+authorsResults = getQueryResults(mostPopularAuthorsQuery)
 printQueryResults(authorsResults, "\tMost Popular Authors")
 
-onePercentErrorDays = getOnePercentErrorDays()
-printErrorQueryResults(onePercentErrorDays, "\tDays With More Than One Percent Failed Hits")
+onePercentErrorDays = getQueryResults(onePercentErrorDaysQuery)
+printErrorQueryResults(onePercentErrorDays, \
+                       "\tDays With More Than One Percent Failed Hits")
+
